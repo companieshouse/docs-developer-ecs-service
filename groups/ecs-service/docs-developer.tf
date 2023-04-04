6f00d4140c6404e14990b273ed4d3b7b230b23f5
Michael Acreman <macreman.ch@gmail.com>
2023-04-04 09:39:52 +0100

    pass in environment and secrets arrays rather than hardcode within the task definition

--- groups/ecs-service/docs-developer.tf
+++ groups/ecs-service/docs-developer.tf
@@ -16,25 +16,7 @@ resource "aws_ecs_task_definition" "docs-developer-task-definition" {
   container_definitions    = <<DEFINITION
     [
         {
-            "environment": [
-                { "name": "DOC_DEVELOPER_SERVICE_PORT", "value": "${local.docs_developer_port}" },
-                { "name": "LOGLEVEL", "value": "${var.log_level}" },
-                { "name": "CDN_HOST", "value": "${var.cdn_host}" },
-                { "name": "CHS_URL", "value": "${var.chs_url}" },
-                { "name": "ACCOUNT_LOCAL_URL", "value": "${var.account_local_url}" },
-                { "name": "DEVELOPER_SPECS_URL", "value": "${var.dev_specs_url}" },
-                { "name": "PIWIK_URL", "value": "${var.piwik_url}" },
-                { "name": "PIWIK_SITE_ID", "value": "${var.piwik_site_id}" },
-                { "name": "REDIRECT_URI", "value": "${var.redirect_uri}" },
-                { "name": "CACHE_POOL_SIZE", "value": "${var.cache_pool_size}" },
-                { "name": "CACHE_SERVER", "value": "${var.cache_server}" },
-                { "name": "COOKIE_DOMAIN", "value": "${var.cookie_domain}" },
-                { "name": "COOKIE_NAME", "value": "${var.cookie_name}" },
-                { "name": "COOKIE_SECURE_ONLY", "value": "${var.cookie_secure_only}" },
-                { "name": "DEFAULT_SESSION_EXPIRATION", "value": "${var.default_session_expiration}" },
-                { "name": "OAUTH2_REDIRECT_URI", "value": "${var.oauth2_redirect_uri}" },
-                { "name": "OAUTH2_AUTH_URI", "value": "${var.oauth2_auth_uri}" }
-            ],
+            "environment": ${jsonencode(local.task_environment)},
             "name": "${local.service_name}",
             "image": "${var.docker_registry}/local/docs.developer.ch.gov.uk:${var.docs_developer_version}",
             "cpu": ${var.required_cpus},
@@ -54,12 +36,7 @@ resource "aws_ecs_task_definition" "docs-developer-task-definition" {
                     "awslogs-stream-prefix": "ecs"
                 }
             },
-            "secrets": [
-                { "name": "CHS_DEVELOPER_CLIENT_ID", "valueFrom": "${local.secrets_arn_map.web-oauth2-client-id}" },
-                { "name": "CHS_DEVELOPER_CLIENT_SECRET", "valueFrom": "${local.secrets_arn_map.web-oauth2-client-secret}" },
-                { "name": "COOKIE_SECRET", "valueFrom": "${local.secrets_arn_map.web-oauth2-cookie-secret}" },
-                { "name": "DEVELOPER_OAUTH2_REQUEST_KEY", "valueFrom": "${local.secrets_arn_map.web-oauth2-request-key}" }
-            ],
+            "secrets": ${jsonencode(local.task_secrets)},
             "volumesFrom": [],
             "essential": true
         }
