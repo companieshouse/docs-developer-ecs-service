dc7a19949bf70646fe169a6b685da634e8b7c506
Michael Acreman <macreman.ch@gmail.com>
2023-04-27 11:29:49 +0100

    increase default memory for java service to avoid out of memory error on startup in ECS

--- groups/ecs-service/variables.tf
+++ groups/ecs-service/variables.tf
@@ -60,7 +60,7 @@ variable "required_cpus" {
 variable "required_memory" {
   type = number
   description = "The required memory for this service"
-  default = 128 # defaulted low for dev environments, override for production
+  default = 256 # defaulted low for java service in dev environments, override for production
 }
 
 # ------------------------------------------------------------------------------
