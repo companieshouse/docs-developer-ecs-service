ac626dd651ba4333233907619afc56fd75613d4c
Jake Metherell <jake@fluffylogic.com>
2023-10-24 11:59:30 +0100

    Override healthcheck path and listener priority to match existing values

--- groups/ecs-service/main.tf
+++ groups/ecs-service/main.tf
@@ -31,6 +31,7 @@ module "ecs-service" {
   lb_listener_arn           = data.aws_lb_listener.dev-site-lb-listener.arn
   lb_listener_rule_priority = local.lb_listener_rule_priority
   lb_listener_paths         = local.lb_listener_paths
+  healthcheck_path          = "/"
 
   # Docker container details
   docker_registry   = var.docker_registry
