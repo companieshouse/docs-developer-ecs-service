f1e58b1c0d3e0b77ce515ffa5e37e440b0bd28a3
Toba Folaranmi <ofolaranmi@companieshouse.gov.uk>
2023-10-27 11:58:29 +0100

    adding env files to task def

--- groups/ecs-service/main.tf
+++ groups/ecs-service/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=feature/ecs-environment-files"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.211"
 
   # Environmental configuration
   environment             = var.environment
